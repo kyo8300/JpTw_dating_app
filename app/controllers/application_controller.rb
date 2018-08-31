a9834f8f222503ce7c0e509b8150b732db09e1ed
kametyou <kametyou777@gmail.com>
2018-08-31 07:33:22 +0000

    redirect new page after sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
+  def after_sign_in_path_for(resource)
+    new_profile_path # ログイン後に遷移するpathを設定
+  end
+  
   private
     def authenticate_user!
       if user_signed_in?
